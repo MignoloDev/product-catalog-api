c00bf3235c74278c3bf2c992fdfc6742ac3b020a
MignoloDev <mignolo93@hotmail.com>
2026-08-05 22:35:32 -0300

    feat: add MakerRequestDTO and MakerResponseDTO

--- src/main/java/com/practice/app/maker/MakerRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package com.practice.app.maker;
-
-public record MakerRequestDTO(String name, String country, String email, String phone) {
-}
--- src/main/java/com/practice/app/maker/MakerResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.practice.app.maker;
-
-public record MakerResponseDTO(Long id, String name, String country, String email, String phone) {
-}
