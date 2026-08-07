e315c0d5062bd99e294bc600723ab4720ba82947
MignoloDev <mignolo93@hotmail.com>
2026-08-07 18:54:46 -0300

    feat: update maker and product routers to plural

--- src/main/java/com/practice/app/maker/MakerController.java
+++ src/main/java/com/practice/app/maker/MakerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/maker")
+@RequestMapping("/makers")
 public class MakerController {
 
     private final MakerService makerService;
